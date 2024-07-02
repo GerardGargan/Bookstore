d386a4139446218de9ad8734bf7979a97acaacd6
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-07-02 14:07:25 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
