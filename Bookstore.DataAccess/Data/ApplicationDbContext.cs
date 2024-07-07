c321b8e06a8d22ca52335b152e9cb0c8a582bf60
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-07-07 21:22:35 +0100

    restructured project

---
+++ Bookstore.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Bookstore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Fiction", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Non-Fiction", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Science Fiction", DisplayOrder = 3 }
+                );
+        }
+    }
+}
---
+++ Bookstore.Models/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bookstore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
