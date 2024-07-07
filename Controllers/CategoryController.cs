a8ea01a1c21878d20813e4c9f62427e2c7c910bf
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-07-07 21:02:54 +0100

    Fixed namespace issues after separating projects

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,4 @@
-﻿using Bookstore.Data;
+﻿using Bookstore.DataAccess.Data;
 using Bookstore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using Bookstore.Data;
+using Bookstore.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
