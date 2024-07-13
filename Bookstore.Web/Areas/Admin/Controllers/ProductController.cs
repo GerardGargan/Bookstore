c666bc554bd71680380329c1fc979b4d9c8e61b7
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-07-13 19:50:09 +0100

    create product controller and index view

---
+++ Bookstore.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Bookstore.DataAccess.Repository.IRepository;
+using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> productList = _unitOfWork.Product.GetAll().ToList();
+            return View(productList);
+        }
+    }
+}
