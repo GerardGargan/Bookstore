c5f1d471d3e19a7bdaaf30c288da234c1c83bfd6
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-07-15 21:42:36 +0100

    add datatable api to product

---
+++ Bookstore.Web/wwwroot/js/product.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        ajax: '/admin/product/getall',
+        "columns": [
+            { data: 'title', "width": "25%" },
+            { data: 'isbn', "width": "15%" },
+            { data: 'listPrice', "width": "15%" },
+            { data: 'author', "width": "15%" },
+            { data: 'category.name', "width": "15%" }
+        ]
+    });
+}
