10e0161ddcf2c23e44d868292472f7b9b79c7a0c
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-07-16 21:23:25 +0100

    add edit and delete buttons to product crud table

--- Bookstore.Web/wwwroot/js/product.js
+++ Bookstore.Web/wwwroot/js/product.js
@@ -8,9 +8,19 @@ function loadDataTable() {
         "columns": [
             { data: 'title', "width": "25%" },
             { data: 'isbn', "width": "15%" },
-            { data: 'listPrice', "width": "15%" },
+            { data: 'listPrice', "width": "10%" },
             { data: 'author', "width": "15%" },
-            { data: 'category.name', "width": "15%" }
+            { data: 'category.name', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/Admin/Product/Upsert/${data}" class="btn btn-primary mx-1"><i class="bi bi-pencil-square"></i> Edit</a>
+                    <a href="/Admin/Product/Delete/${data}" class="btn btn-danger mx-1"><i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+                },
+                "width": "25%"
+            }
         ]
     });
 }
