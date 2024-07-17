8c63c4ca4f5114185b0699a30676803e913d0276
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-07-17 22:25:53 +0100

    scaffold Identity

--- Bookstore.DataAccess/Data/ApplicationDbContext.cs
+++ Bookstore.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Bookstore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookstore.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -14,6 +16,8 @@ namespace Bookstore.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Fiction", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "Non-Fiction", DisplayOrder = 2 },
--- Bookstore.Web/Program.cs
+++ Bookstore.Web/Program.cs
@@ -2,12 +2,15 @@ using Bookstore.DataAccess.Data;
 using Bookstore.DataAccess.Repository;
 using Bookstore.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
