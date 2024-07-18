00976e1bad7a24fe76cbac78d051092253598ba5
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-07-18 20:55:30 +0100

    add application user migration

--- Bookstore.DataAccess/Data/ApplicationDbContext.cs
+++ Bookstore.DataAccess/Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace Bookstore.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
