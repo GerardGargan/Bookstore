ec82d2fbde141fb5dbdf73f5615c7d22c9a9ca22
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-07-20 16:56:21 +0100

    add more columns to registration form

--- Bookstore.Models/ApplicationUser.cs
+++ Bookstore.Models/ApplicationUser.cs
@@ -11,7 +11,7 @@ namespace Bookstore.Models
     public class ApplicationUser : IdentityUser
     {
         [Required]
-        public int Name { get; set; }
+        public string Name { get; set; }
         public string? StreetAddress { get; set; }
         public string? City { get; set; }
         public string? County {  get; set; }
