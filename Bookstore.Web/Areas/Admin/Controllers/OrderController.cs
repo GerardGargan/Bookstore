13dac5c454f9de0dded23975f736165b102edcee
Gerard Gargan <gerardgargan@hotmail.co.uk>
2024-07-31 21:39:32 +0100

    add order controller and view

---
+++ Bookstore.Web/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using Bookstore.DataAccess.Repository.IRepository;
+using Bookstore.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreWeb.Areas.Admin.Controllers
+{
+	[Area("admin")]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+			_unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+
+		#region API CALLS
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> allOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+			return Json(new { data = allOrderHeaders });
+		}
+
+		#endregion
+	}
+
+}
---
+++ Bookstore.Web/wwwroot/js/order.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        ajax: '/admin/order/getall',
+        "columns": [
+            { data: 'id', "width": "5%" },
+            { data: 'name', "width": "15%" },
+            { data: 'phoneNumber', "width": "20%" },
+            { data: 'applicationUser.email', "width": "15%" },
+            { data: 'orderStatus', "width": "10%" },
+            { data: 'orderTotal', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/Admin/order/details?orderId=${data}" class="btn btn-primary mx-1"><i class="bi bi-pencil-square"></i></a>`
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}
