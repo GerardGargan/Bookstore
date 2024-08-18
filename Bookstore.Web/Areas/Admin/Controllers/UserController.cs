837bf3f8d854e7b1bbc2d6de936a055fb9d75556
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-08-18 17:22:01 +0100

    add manage user datatable

---
+++ Bookstore.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Bookstore.DataAccess.Repository.IRepository;
+using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> allUsers = _unitOfWork.User.GetAll().ToList();
+            return Json(new { data = allUsers });
+        }
+
+        #endregion
+
+    }
+}
